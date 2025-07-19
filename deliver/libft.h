c79ae25cf2372285cd1e4fb707f89df8f3c7dea0
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-07-19 14:08:11 -0300

    add ft_isascii

--- deliver/libft.h
+++ deliver/libft.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 14:05:33 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/07/19 14:06:09 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
 
 #endif 
--- deliver/testing.c
+++ deliver/testing.c
@@ -2,22 +2,31 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main(void)
+int	main(void)
 {
-    int     res;
-    char    t_isalpha;
-    char    t_isdigit;
-    char    t_alnum;
+	int		res;
+	char	t_isalpha;
+	char	t_isdigit;
+	char	t_isalnum;
+	char	t_isascii;
 
-    t_isalpha = 'F';
-    res = ft_isalpha(t_isalpha);
-    printf("result: %i\n", res);
+    //ft_isalpha()
+	t_isalpha = 'F';
+	res = ft_isalpha(t_isalpha);
+	printf("ft_isalpha(): %i\n", res);
 
-    t_isdigit = '2';
-    res = ft_isdigit(t_isdigit);
-    printf("result: %i\n", res);
+    //ft_isdigit()
+	t_isdigit = '2';
+	res = ft_isdigit(t_isdigit);
+	printf("ft_isdigit(): %i\n", res);
 
-    t_alnum = '!';
-    res = ft_isalnum(t_alnum);
-    printf("result: %i\n", res);
+    //ft_isalnum()
+	t_isalnum = '!';
+	res = ft_isalnum(t_isalnum);
+	printf("ft_isalnum(): %i\n", res);
+
+    //ft_isascii()
+	t_isascii = '~';
+	res = isascii(t_isascii);
+	printf("isascii(): %i\n", res);
 }
