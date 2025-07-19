a4d2670985ec9b29504d0f2fca5a9e679ba05f63
GiovanniBernardesFigueiredo <giovannibernardesfigueiredo@gmail.com>
2025-07-19 00:50:05 -0300

    add ft_isalnum()

--- deliver/testing.c
+++ deliver/testing.c
@@ -7,6 +7,7 @@ int main(void)
     int res;
     char t_isalpha;
     char    t_isdigit;
+    char    t_alnum;
 
     t_isalpha = 'F';
     res = ft_isalpha(t_isalpha);
@@ -14,5 +15,9 @@ int main(void)
 
     t_isdigit = '2';
     res = ft_isdigit(t_isdigit);
+    printf("result: %i\n", res);
+
+    t_alnum = '!';
+    res = ft_isalnum(t_alnum);
     printf("result: %i\n", res);
 }
