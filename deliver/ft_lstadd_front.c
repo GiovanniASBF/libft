4c62604ecdb4c0567853b810911414997fbb6dc3
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-07-30 16:28:15 -0300

    fix: Makefile

--- deliver/ft_lstadd_front.c
+++ deliver/ft_lstadd_front.c
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/28 18:03:01 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/07/30 14:42:17 by gaguiar-         ###   ########.fr       */
+/*   Created: 2025/07/30 14:29:06 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/07/30 14:56:51 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	unsigned int	i;
-	unsigned int	len;
-	char			*str;
-
-	i = 0;
-	len = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (NULL);
-	while (i < len)
-	{
-		str[i] = f(i, s[i]);
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	new->next = &*lst;
 }
+
+/*
+lst: The address of a pointer to the first node of
+a list.
+new: The address of a pointer to the node to be
+added.
+
+Adds the node ’new’ at the beginning of the list.
+*/
--- deliver/libft.h
+++ deliver/libft.h
@@ -6,7 +6,7 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 14:05:33 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/07/30 12:09:46 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/07/30 14:49:45 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *v2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
