bd85d694f5686d270f7be29977e6ef95c195c712
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-07-31 16:04:42 -0300

    add ft_lstdelone

--- deliver/libft.h
+++ deliver/libft.h
@@ -6,7 +6,7 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 14:05:33 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/07/31 15:46:37 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/07/31 16:02:42 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,7 @@ int		ft_toupper(int c);
 
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
